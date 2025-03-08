03998f97e49c9a15ce90766fdee3be44a0d26ffb
jmmach <jmmachan@mcm.edu.ph>
2025-03-08 23:17:35 +0800

    Create Machan_PersonalDetails.java

---
+++ Machan_PersonalDetails.java
@@ -0,0 +1,13 @@
+ public class Machan_PersonalDetails {
+    public static void main(String[] args) {
+        String name = "Jasmine Mikaela Machan";
+        int age = 14;
+        double height = 1.60;
+        boolean isStudent = true;
+ 
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " meters");
+        System.out.println("Is a Student? " + isStudent);
+    }
+}
