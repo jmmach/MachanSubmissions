8cfbcf9d5451d9ad9d0a2386a8262d297ab8c1f1
jmmach <jmmachan@mcm.edu.ph>
2025-03-08 22:42:56 +0800

    Create Machan_Calculator.java

---
+++ Machan_Calculator.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Machan_Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Prompt user for input
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+        
+        // Perform calculations
+        double addition = num1 + num2;
+        double subtraction = num1 - num2;
+        double multiplication = num1 * num2;
+        double division = num1 / num2;
+        
+        // Display results
+        System.out.println("Results:");
+        System.out.println("Addition: " + addition);
+        System.out.println("Subtraction: " + subtraction);
+        System.out.println("Multiplication: " + multiplication);
+        System.out.println("Division: " + division);
+        
+        scanner.close();
+    }
+}
